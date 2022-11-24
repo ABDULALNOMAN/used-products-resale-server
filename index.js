7807fbdf53d2acdf6b4ed9d78ac91192273f9710
unknown <nomanmd1st2001@gmail.com>
2022-11-24 23:39:53 +0600

    add crud

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const port = process.env.PORT||5000
+const port = process.env.PORT || 5000
+require('dotenv').config()
 
 
 // middleware
-app = (cors())
-app = (express.json())
+app.use(cors())
+app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.epy9glr.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@cluster0.epy9glr.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const run = async() => {
+    try {
+        const userServices = client.db('gsmarea').collection('category')
+
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const result = await userServices.find(query).toArray()
+            res.send(result)
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(error=>console.log(error))
+
 
 
 
