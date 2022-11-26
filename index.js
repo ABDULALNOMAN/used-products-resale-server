e983265d7fdfad66a5622293a37f8d3d66672a89
unknown <nomanmd1st2001@gmail.com>
2022-11-26 15:44:49 +0600

    user identify insert

--- index.js
+++ index.js
@@ -12,16 +12,42 @@ app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@cluster0.epy9glr.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async() => {
     try {
-        const userServices = client.db('gsmarea').collection('category')
+        const userServices = client.db("gsmarea").collection('category')
+        const servicesProducts = client.db('gsmarea').collection('products')
+        const userBooking = client.db('gsmarea').collection('booking')
+        const userInformation = client.db('gsmarea').collection('identity')
 
         app.get('/services', async(req, res) => {
             const query = {}
             const result = await userServices.find(query).toArray()
             res.send(result)
+        })
+        app.get('/category/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {categoryId:id}
+            const result = await servicesProducts.find(query).toArray()
+            res.send(result)
+        })
+        app.post('/bookitem',async(req,res)=>{
+            const data = req.body
+            const result = await userBooking.insertOne(data)
+            res.send(result)
+        })
+        app.post('/userstore', async(req,res) => {
+            const data = req.body;
+            const result = await userInformation.insertOne(data)
+            res.send(result)
+            console.log(result)
+        })
+        app.get('/users', async (req, res) => {
+            const email = req.query.email
+            const result = await userInformation.findOne(email)
+            if (result) {
+                return res.send(result)
+            }
         })
     }
     finally {
